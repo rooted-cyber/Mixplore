c6c3d081699e5df80ae5da6099700ce7717c556a
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-12-24 21:36:45 +0530

    Update blast.py

--- blast.py
+++ blast.py
@@ -3,5 +3,4 @@ sudo("apt update")
 sudo("apt upgrade -y")
 sudo("apt install wget python python3-pip -y")
 sudo("wget https://gist.githubusercontent.com/rooted-cyber/05ca0b015cdbdb3ac87bb9cdaed3da75/raw/tu")
-bash tu ins
-bash tu st
+sudo("bash tu ins:bash tu st")
