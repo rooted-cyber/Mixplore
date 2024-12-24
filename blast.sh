9bfa5a064a3a12faf5c629dc32449219b1d9d802
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-12-24 21:05:02 +0530

    Update blast.sh

--- blast.sh
+++ blast.sh
@@ -1,1 +1,1 @@
-sudo curl -L https://gist.githubusercontent.com/rooted-cyber/44a41ef2892b2c007190880b2f7b7090/raw/489593eb87aeaead33f3afbd2e7822a3907d49cd/ullt | bash
+curl -L https://gist.githubusercontent.com/rooted-cyber/44a41ef2892b2c007190880b2f7b7090/raw/489593eb87aeaead33f3afbd2e7822a3907d49cd/ullt | bash
