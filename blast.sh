775f6e3d0319f09ce5de475977c9ea3e6cea2495
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-12-24 21:14:28 +0530

    Update blast.sh

--- blast.sh
+++ blast.sh
@@ -1,3 +1,6 @@
+apt update
+apt upgrade -y
+apt install wget python python3-pip -y
 wget https://gist.githubusercontent.com/rooted-cyber/05ca0b015cdbdb3ac87bb9cdaed3da75/raw/tu
 bash tu ins
 bash tu st
