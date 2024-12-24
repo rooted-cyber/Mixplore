daa77edc63374e08f240f0a9a0d38a062d3c0bab
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-12-24 21:32:01 +0530

    Update blast.sh

--- blast.sh
+++ blast.sh
@@ -1,6 +1,7 @@
-sudo apt update
-sudo apt upgrade -y
-sudo apt install wget python python3-pip -y
-sudo wget https://gist.githubusercontent.com/rooted-cyber/05ca0b015cdbdb3ac87bb9cdaed3da75/raw/tu
+from os import system as sudo
+sudo("apt update")
+sudo("apt upgrade -y")
+sudo("apt install wget python python3-pip -y")
+sudo("wget https://gist.githubusercontent.com/rooted-cyber/05ca0b015cdbdb3ac87bb9cdaed3da75/raw/tu")
 bash tu ins
 bash tu st
